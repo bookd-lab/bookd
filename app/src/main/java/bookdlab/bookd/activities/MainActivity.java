ffaecc7e7b80e55948f08f7f301e8bf2c797b36d
Pranav Konduru <dev.squarerock@gmail.com>
2016-11-16 11:58:53 -0800

    firebase auth v1. support for email and google

--- app/src/main/java/bookdlab/bookd/activities/MainActivity.java
+++ app/src/main/java/bookdlab/bookd/activities/MainActivity.java
@@ -1,17 +1,21 @@
 package bookdlab.bookd.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 import bookdlab.bookd.R;
 import bookdlab.bookd.adapters.HomeTabsAdapter;
@@ -27,12 +31,22 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private static final int RC_SIGN_IN = 1001;
+
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        // Setup Firebase
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        setupAuthStateListener();
 
         setSupportActionBar(toolbar);
         if(null != getSupportActionBar()) {
@@ -52,5 +66,55 @@ public class MainActivity extends AppCompatActivity {
         searchView.setQueryHint(getResources().getString(R.string.search_hint));
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mAuthStateListener != null){
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult: ");
+        if(requestCode == RC_SIGN_IN){
+            Log.d(TAG, "onActivityResult: RC_SIGN_IN");
+            if(resultCode == RESULT_OK){
+                Log.d(TAG, "onActivityResult: Result is okay");
+                Toast.makeText(this, "Signed in", Toast.LENGTH_LONG).show();
+            } else if(resultCode == RESULT_CANCELED){
+                Log.d(TAG, "onActivityResult: Result is cancelled");
+                Toast.makeText(this, "Signing cancelled", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void setupAuthStateListener() {
+        Log.d(TAG, "setupAuthStateListener: ");
+        mAuthStateListener = firebaseAuth -> {
+            if (firebaseAuth.getCurrentUser() != null) {
+                Log.d(TAG, "setupAuthStateListener: showing splash screen");
+            } else {
+                Log.d(TAG, "setupAuthStateListener: starting firebase auth ui");
+                startActivityForResult(AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(false)
+                        .setProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                        .build(), RC_SIGN_IN);
+            }
+        };
     }
 }
