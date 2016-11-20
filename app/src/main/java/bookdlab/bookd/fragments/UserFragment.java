6ffdf314c8079b20109901efc03fd2182fbd5955
Rubab Uddin <uddin.rubab@gmail.com>
2016-11-20 14:19:29 -0800

    set and get user parameters from User fragment
    Added onfocus listeners for easier editing

--- app/src/main/java/bookdlab/bookd/fragments/UserFragment.java
+++ app/src/main/java/bookdlab/bookd/fragments/UserFragment.java
@@ -3,21 +3,22 @@ package bookdlab.bookd.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import bookdlab.bookd.R;
-import bookdlab.bookd.adapters.BusinessAdapter;
-import bookdlab.bookd.api.BusinessesClient;
-import bookdlab.bookd.models.Business;
+import bookdlab.bookd.models.User;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by rubab.uddin on 11/19/2016.
@@ -25,20 +26,38 @@ import butterknife.ButterKnife;
 
 public class UserFragment extends Fragment {
 
+    @BindView(R.id.tvUsername) TextView tvUsername;
     @BindView(R.id.tvMemberSince) TextView tvMemberSince;
+    @BindView(R.id.tvEmail) TextView tvEmail;
+    @BindView(R.id.ivUserProfileImage) CircleImageView ivUserProfileImage;
 
-    //TODO: inject properly
-    BusinessesClient businessesClient = new BusinessesClient();
+    @BindView(R.id.vsAbout) ViewSwitcher vsAbout;
+    @BindView(R.id.tvAbout) TextView tvAbout;
+    @BindView(R.id.etAbout) EditText etAbout;
+
+    @BindView(R.id.vsLocation) ViewSwitcher vsLocation;
+    @BindView(R.id.tvLocation) TextView tvLocation;
+    @BindView(R.id.etLocation) EditText etLocation;
+
+    @BindView(R.id.vsPhoneNumber) ViewSwitcher vsPhoneNumber;
+    @BindView(R.id.tvPhoneNumber) TextView tvPhoneNumber;
+    @BindView(R.id.etPhoneNumber) EditText etPhoneNumber;
+
+    FirebaseUser fUser;
+    User user = new User(); //get the actual logged in user
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fUser = FirebaseAuth.getInstance().getCurrentUser();
+
     }
 
-    public static ExploreFragment newInstance() {
-        ExploreFragment fragment = new ExploreFragment();
+    public static UserFragment newInstance() {
+        UserFragment fragment = new UserFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -46,14 +65,16 @@ public class UserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, container, false);
+
         ButterKnife.bind(this, view);
 
-        businessList = new ArrayList<>();
-        businessAdapter = new BusinessAdapter(getActivity(), businessList);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(businessAdapter);
+        tvUsername.setText(user.getUsername());
+        tvMemberSince.setText(user.getPhoneNumber());
+        tvEmail.setText(user.getEmail());
+        Glide.with(this).load(user.getProfileImageURL()).into(ivUserProfileImage);
+        tvAbout.setText(user.getAbout());
+        tvLocation.setText(user.getAddress());
+        tvPhoneNumber.setText(user.getPhoneNumber());
 
         return view;
     }
@@ -62,8 +83,44 @@ public class UserFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        List<Business> res = businessesClient.findBusinesses("");
-        businessList.addAll(res);
-        businessAdapter.notifyDataSetChanged();
+    }
+
+    public void AboutClicked() {
+        vsAbout.showNext();
+        etAbout.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String newAbout = etAbout.getText().toString();
+                user.setAbout(newAbout);
+                tvAbout.setText(newAbout);
+                vsAbout.showPrevious();
+            }
+        });
+    }
+
+    public void LocationClicked() {
+        vsLocation.showNext();
+        etLocation.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String newLocation = etLocation.getText().toString();
+                user.setAddress(newLocation);
+                tvLocation.setText(newLocation);
+                vsLocation.showPrevious();
+            }
+        });
+    }
+
+    public void PhoneNumberClicked() {
+        vsPhoneNumber.showNext();
+        etPhoneNumber.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String newPhoneNumber = etPhoneNumber.getText().toString();
+                user.setPhoneNumber(newPhoneNumber);
+                tvPhoneNumber.setText(newPhoneNumber);
+                vsPhoneNumber.showPrevious();
+            }
+        });
     }
 }
