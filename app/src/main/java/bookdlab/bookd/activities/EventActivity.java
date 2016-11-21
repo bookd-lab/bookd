be9a1a98213f2f3380732aacf21388f0e143371b
Rubab Uddin <uddin.rubab@gmail.com>
2016-11-20 20:01:30 -0800

    Events clickable from Events tab

--- app/src/main/java/bookdlab/bookd/activities/EventActivity.java
+++ app/src/main/java/bookdlab/bookd/activities/EventActivity.java
@@ -6,17 +6,22 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
+import org.parceler.Parcels;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import bookdlab.bookd.R;
 import bookdlab.bookd.adapters.BusinessAdapter;
 import bookdlab.bookd.models.Business;
+import bookdlab.bookd.models.Event;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class EventActivity extends AppCompatActivity {
+
+    public static final String EXTRA_EVENT = "EXTRA_EVENT";
 
     @BindView(R.id.tvEventName)
     TextView tvEventName;
@@ -27,7 +32,7 @@ public class EventActivity extends AppCompatActivity {
     @BindView(R.id.bookingsRV)
     RecyclerView bookingsRV;
 
-
+    Event eventData;
     private List<Business> bookingsList;
     private BusinessAdapter businessAdapter;
 
@@ -38,14 +43,23 @@ public class EventActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        eventData = Parcels.unwrap(getIntent().getParcelableExtra(EXTRA_EVENT));
+
         bookingsList = new ArrayList<>();
         businessAdapter = new BusinessAdapter(this, bookingsList);
 
+        initData();
+
+    }
+
+    private void initData(){
+        tvEventName.setText(eventData.getName());
+        tvDate.setText(eventData.getDates());
+        //tvTags.setText(eventData.getTags());
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         bookingsRV.setLayoutManager(layoutManager);
         bookingsRV.setAdapter(businessAdapter);
         //loadingIndicatorView.show();
-
     }
 
 
